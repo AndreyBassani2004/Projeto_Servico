081035322391a7ff7c2ff2b0b504fb412ef16412
AndreyBassani2004 <83016044+AndreyBassani2004@users.noreply.github.com>
2022-08-14 09:09:22 -0300

    Criação da pagina denunciar avaliação parte I.

--- Projeto_Servico/src/main/java/Servlets/ServletCarregarRequisicao.java
+++ Projeto_Servico/src/main/java/Servlets/ServletCarregarRequisicao.java
@@ -200,6 +200,28 @@ public class ServletCarregarRequisicao extends HttpServlet {
 					request.getRequestDispatcher("principal/erro404.jsp").forward(request, response);
 				}
 				
+			}else if (acao != null && !acao.isEmpty() && acao.equalsIgnoreCase("denunciarAvaliacao")) {
+			
+				String id_anuncio = request.getParameter("id");
+				
+				String id_user = request.getParameter("id_user");
+
+				HttpSession session = request.getSession();
+
+				String usuarioLogado = (String) session.getAttribute("usuario");
+
+				Long id_usuario = daoUsuarioPosLogin.consultaUsuarioLogado(usuarioLogado).getId();
+
+				String perfil = daoUsuarioPosLogin.consultaUsuarioLogado(usuarioLogado).getPerfil();
+
+				if (id_usuario.equals(Long.parseLong(id_user)) && perfil.equals("PRESTADOR")) {
+					
+					System.out.println(id_anuncio);
+					
+					request.getRequestDispatcher("principal/denunciarAvaliacao.jsp").forward(request, response);
+				}else {
+					request.getRequestDispatcher("principal/erro404.jsp").forward(request, response);
+				}			
 			}else {
 				request.getRequestDispatcher("principal/principal.jsp").forward(request, response);
 
---
+++ Projeto_Servico/src/main/webapp/principal/denunciarAvaliacao.jsp
@@ -0,0 +1,31 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+	pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<link rel="stylesheet" type="text/css"
+	href="<%=request.getContextPath()%>/principal/css/denunciarAvaliacaoStyle.css">
+<link rel="stylesheet"
+	href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css"
+	integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
+	crossorigin="anonymous">
+<title>PresTec</title>
+</head>
+<body>
+
+	<%@include file="/principal/Componente/header2.jsp"%>
+
+	<br/><br/><br/><br/><br/><br/>
+		
+	<div class="container">
+		<div class="row"><h2>Denunciar Avaliação</h2></div>
+	</div>
+	
+	
+	<br/><br/><br/><br/><br/><br/>
+	<%@include file="/Componentes/rodape.jsp"%>
+
+
+</body>
+</html>
